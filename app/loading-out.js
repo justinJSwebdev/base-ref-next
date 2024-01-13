2da7eb6825dddee7949f241c22f7f929f5c28a5e
justinJSwebdev <trankyhung225@gmail.com>
2024-01-13 19:56:09 +0700

    base

---
+++ app/loading-out.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import css from "./loading.module.css"
+export default function LoadingContent() {
+    return (
+        <div className={css.loading}>Loading Data</div>
+    )
+}
